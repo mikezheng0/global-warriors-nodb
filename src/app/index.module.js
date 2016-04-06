ef4ed0691764a3b1988633a3361abab131ab250f
Mike Zheng <mike.zheng@mohawkcollege.ca>
2016-04-06 18:03:01 -0400

    add fight cards to the main page

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('globalWarriorsNodb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+    .module('globalWarriorsNodb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr', 'youtube-embed']);
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,7 +6,29 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout) {
-    
+  function MainController($timeout, $scope) {
+    var video = [
+      {
+        id: 0,
+        title: 'Global Warriors 2 Main Event Josh Hill vs Josh Rettinghouse',
+        link: 'Q4mcfgXIFBk'
+      }, {
+        id: 1,
+        title: '3 Josh Rich vs Dennis Puric',
+        link: 'n3iCIABzhmE '
+      }, {
+        id: 2,
+        title: '1 Hale vs Sumatri',
+        link: 'F6feewQsUBs'
+      }, {
+        id: 3,
+        title:'Global Warriors 2 Burlington, Ontario Co Main Event Ryan Dickson vs Jason Witt',
+        link: '55l3D1Eic_E'
+      }
+    ];
+    $scope.mainVideo = video[2].link;
+    $scope.setVideo = function(id){
+      $scope.mainVideo = video[id].link;
+    }
   }
 })();
