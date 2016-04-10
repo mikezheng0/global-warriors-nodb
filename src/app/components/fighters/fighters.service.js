c6989db44f82ee083239ba9a4ddd040c18ccd268
Matt Lee <matt_lee07@live.ca>
2016-04-10 02:17:08 -0400

    Added content to fighter details page and ng-click to fighters page, need to fix the route for fighter details

--- src/app/components/fighters/fighters.service.js
+++ src/app/components/fighters/fighters.service.js
@@ -12,42 +12,58 @@
         'id': 0,
         'name':'Dave Hale',
         'image':'assets/images/some-guy.jpg',
-        'blurb':'Some guy fights'
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       },{
         'id': 1,
         'name':'Josh Hill',
         'image':'assets/images/josh-hill.jpg',
-        'blurb':''
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       },{
         'id': 2,
         'name':'Josh Hill',
         'image':'assets/images/josh-hill.jpg',
-        'blurb':''
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       },{
         'id': 3,
         'name':'Will Romero',
         'image':'assets/images/will-romero.jpg',
-        'blurb':''
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       },{
         'id': 4,
         'name':'Will Romero',
         'image':'assets/images/will-romero.jpg',
-        'blurb':''
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       },{
         'id': 5,
         'name':'Will Romero',
         'image':'assets/images/will-romero.jpg',
-        'blurb':''
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       },{
         'id': 6,
         'name':'Will Romero',
         'image':'assets/images/will-romero.jpg',
-        'blurb':''
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       },{
         'id': 7,
         'name':'Will Romero',
         'image':'assets/images/will-romero.jpg',
-        'blurb':''
+        'height':'5\'10\"',
+        'weight': '135lbs',
+        'record': '10-0-0'
       }
     ];
 
